7acaf367900245520d9bfc4f5286dc02c2b06f21
drake <=>
2019-03-20 10:56:25 -0400

    change start script

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.get("/", (req, res) => res.json({msg: 'ok'}))
 app.use(logger('dev'));
 app.use(bodyParser.json());
 
-app.listen(3000, () => console.log('up and running'))
+app.listen(PORT, () => console.log('up and running'))
